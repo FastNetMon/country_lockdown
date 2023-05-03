a652c43e45c7ff3786ca7385f7ad94c7acf1990f
Pavel Odintsov <pavel.odintsov@gmail.com>
2023-05-03 17:47:03 +0100

    Added logic to show type of GeoIP database

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
-import "log"
-import "flag"
-import "github.com/oschwald/geoip2-golang"
+import (
+	"flag"
+	"log"
+
+	"github.com/oschwald/geoip2-golang"
+)
 
 var geoip_country_maxmind_db *geoip2.Reader
 
@@ -11,15 +14,21 @@ var country_geoip_path = flag.String("geoip_path", "/usr/share/GeoIP/GeoIP2-Coun
 func main() {
 	flag.Parse()
 
-        // GeoIP for countries
+	// GeoIP for countries
 	geoip_country_maxmind_db, err := geoip2.Open(*country_geoip_path)
 
-        if err != nil {
-                log.Fatalf("Can't open country mapping file: %v", err)
-        }
+	if err != nil {
+		log.Fatalf("Can't open country mapping file: %v", err)
+	}
 
-        defer geoip_country_maxmind_db.Close()
+	defer geoip_country_maxmind_db.Close()
 
+	log.Printf("Loaded GeoIP file: %+v", geoip_country_maxmind_db.Metadata())
 
-	log.Printf("Hi")
+	// We need to be sure that database has correct type
+	if geoip_country_maxmind_db.Metadata().DatabaseType != "GeoIP2-Country" {
+		log.Fatalf("Wrong type of GeoIP database %s, please GeoIP2-Country type", geoip_country_maxmind_db.Metadata().DatabaseType)
+	}
+
+	log.Printf("GeoIP database has correct format")
 }
