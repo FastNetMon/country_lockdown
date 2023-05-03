7d8806437a5b93ab8021379d4687b1022c046630
Pavel Odintsov <pavel.odintsov@gmail.com>
2023-05-03 18:51:35 +0100

    Added logic to load traffic per country

--- main.go
+++ main.go
@@ -32,12 +32,12 @@ func main() {
 
 	log.Printf("GeoIP database has correct format")
 
-	load_all_ipv4_networks(geoip_country_maxmind_db)
-
+	load_all_ipv4_networks_for_country(geoip_country_maxmind_db, "CN")
 }
 
-// Loads all networks
-func load_all_ipv4_networks(geoip_country_maxmind_db *maxminddb.Reader) error {
+// Loads all networks for country with specific ISO code
+// Luckily for us Hong Kong has HK code here and China has CN
+func load_all_ipv4_networks_for_country(geoip_country_maxmind_db *maxminddb.Reader, country_iso_code string) error {
 	// All fields https://github.com/oschwald/geoip2-golang/blob/main/reader.go#L139
 	record := geoip2.Country{}
 
@@ -49,6 +49,8 @@ func load_all_ipv4_networks(geoip_country_maxmind_db *maxminddb.Reader) error {
 	// separately. To only iterate over the IPv4 networks once, use the
 	// SkipAliasedNetworks option.
 	networks := geoip_country_maxmind_db.Networks(maxminddb.SkipAliasedNetworks)
+
+	number_of_networks := 0
 
 	for networks.Next() {
 		subnet, err := networks.Network(&record)
@@ -63,17 +65,25 @@ func load_all_ipv4_networks(geoip_country_maxmind_db *maxminddb.Reader) error {
 			continue
 		}
 
+		// TODO:
 		// We do not expect private ranges here but we have to be sure
 		// Well, I do not think that we have any functions to do so for prefixes
 		// Skip for now
 
-		// Luckily for us Hong Kong has HK code here and China has CN
-		fmt.Printf("%s: %s\n", subnet.String(), record.Country.IsoCode)
+		// Check that network belongs to country we're interested in
+		if record.Country.IsoCode != country_iso_code {
+			continue
+		}
+
+		// fmt.Printf("%s: %s\n", subnet.String(), record.Country.IsoCode)
+		number_of_networks++
 	}
 
 	if networks.Err() != nil {
 		return fmt.Errorf("Cannot correctly iterate over all available networks %w", networks.Err())
 	}
+
+	log.Printf("Successfully loaded %d prefixes for country %s", number_of_networks, country_iso_code)
 
 	return nil
 }
