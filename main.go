0f5ef18abf0b9b2b9ac7f0d379c7c3d1dc63c7ea
Pavel Odintsov <pavel.odintsov@gmail.com>
2023-05-03 17:33:23 +0100

    Added configuration to set path to GeoIP file

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import "log"
+import "flag"
 import "github.com/oschwald/geoip2-golang"
 
 var geoip_country_maxmind_db *geoip2.Reader
 
+var country_geoip_path = flag.String("geoip_path", "/usr/share/GeoIP/GeoIP2-Country.mmdb", "Path to GeoIP2 MMDB country data file")
+
 func main() {
+	flag.Parse()
+
         // GeoIP for countries
-	geoip_country_maxmind_db, err := geoip2.Open("/usr/share/GeoIP/GeoIP2-Country.mmdb")
+	geoip_country_maxmind_db, err := geoip2.Open(*country_geoip_path)
 
         if err != nil {
                 log.Fatalf("Can't open country mapping file: %v", err)
